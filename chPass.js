a37b27f50eb4997f2f6206f8d363ee0a0349ccbe
S3V04KA <sevstar2206@gmail.com>
2023-03-17 20:05:03 +0500

    123

--- chPass.js
+++ chPass.js
@@ -3,23 +3,23 @@ function chPass() {
     const i2 = document.getElementById('password2')
     const i3 = document.getElementById('password3')
 
-    if(i3.value !== i2.value){
+    if (i3.value !== i2.value) {
         const errorM = document.getElementById('errorM');
         errorM.style.display = 'block';
         return
     }
 
-    const query = {
+    const query = JSON.stringify({
         query: `mutation changePass {
         changePassword(
             lastPass: \"${i1.value}\"
             newPass: \"${i2.value}\"
         )
-      }`}
+      }`});
 
     makeRequest(query, getCookie('userKey')).then(res => {
         console.log(res)
-        if(res.errors && res.errors.message === 'Wrong Password'){
+        if (res.errors && res.errors.message === 'Wrong Password') {
             const errorM = document.getElementById('errorM');
             errorM.style.display = 'block';
             return
